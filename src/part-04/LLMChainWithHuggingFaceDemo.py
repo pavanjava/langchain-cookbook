ac872eda74cc5f8d9378138d2820ac5d24a0d970
Pavan Mantha <manthapavankumar11@gmail.com>
2023-07-19 13:50:36 +0530

    implemented the LangChain's LLMChain with HuggingFaceHub API and Pipeline Methods

--- src/part-04/LLMChainWithHuggingFaceDemo.py
+++ src/part-04/LLMChainWithHuggingFaceDemo.py
@@ -3,14 +3,18 @@ from src.util import setEnv
 
 setEnv()
 
-template = """Question: {question} expand the answer in about 20 sentences
-Answer: """
+template = """Question: {question}
+Answer:  """
+
+wikiSQL = 'mrm8488/t5-base-finetuned-wikiSQL'
+flan_t5 = 'google/flan-t5-xxl'
+databricks_dolly = 'databricks/dolly-v2-3b'
 
 prompt = PromptTemplate(template=template, input_variables=['question'])
-llm = HuggingFaceHub(repo_id='google/flan-t5-xxl', model_kwargs={'temperature': 0.5, 'max_length': 128})
+llm = HuggingFaceHub(repo_id=databricks_dolly, model_kwargs={'temperature': 0.5, 'max_length': 128})
 
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-question = "What is Area 51 ?"
+question = "What are transformers in NLP ?"
 
 print(llm_chain.run(question))
